886836b2d3d74c06da4abb92203dcbcc11a0e4b5
梁惠涌 <lianghuiyong@outlook.com>
2019-06-19 12:44:22 +0800

    openresty

--- doc_openresty/shell/entrypoint.sh
+++ doc_openresty/shell/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# 启动openresty  -c /root/conf/nginx.conf
-openresty -c /nginx.conf
+# 启动openresty
+# openresty -c /root/conf/nginx.conf
 
 # 启动frpc
 # ./frp_0.21.0_linux_amd64/frpc -c /frpc.ini
